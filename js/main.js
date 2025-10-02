4d9c18b0dd50f4cb795f7741ae18ea07857a4fcc
Eslam Mohamed <eslama202002@gmail.com>
2025-10-02 09:35:03 +0300

    Complete Responsive design , adding some animation and switched on dark mode

--- js/main.js
+++ js/main.js
@@ -13,6 +13,13 @@
 // },{threshold:0.2})
 // myAnimation.forEach(el=>{animationObserver.observe(el)})
 
+if (localStorage.getItem("theme") == "dark") {
+    document.body.classList.add("dark")
+}
+else{
+        document.body.classList.remove("dark")
+
+}
 function createObserver(selector,animationStyle){
 let myAnimation = document.querySelectorAll(selector)
 const animationObserver = new IntersectionObserver((entries)=>{
@@ -32,3 +39,49 @@ createObserver(".from-right","animate-slide-in-right")
 createObserver(".from-left","animate-slide-in-left")
 
 
+let mode = document.querySelectorAll(".mode")
+
+
+mode.forEach((btn)=>{
+
+
+
+    btn.addEventListener("click",function(e){
+        console.log(e.target);
+        
+        document.body.classList.toggle("dark")
+        if (document.body.classList.contains("dark")) {
+            localStorage.setItem("theme","dark")
+        }
+        else{
+            localStorage.setItem("theme","light")
+        }
+        
+        if (e.target.classList.contains("fa-moon")) {
+            console.log(e.target);
+            
+            e.target.classList.remove("fa-moon")
+            e.target.classList.add("fa-sun")   
+        }
+        else {
+            e.target.classList.remove("fa-sun")
+            e.target.classList.add("fa-moon")  
+            console.log(e.target);
+            
+        }
+    })
+    
+})
+    
+let mobileMenu = document.querySelector(".mobile-menu")
+let menuBar = document.querySelector(".menu-bar")
+menuBar.addEventListener("click",function(){
+    if (mobileMenu.classList.contains("-translate-x-full")) {
+        
+        mobileMenu.classList.remove("-translate-x-full")
+    }
+else{
+            mobileMenu.classList.add("-translate-x-full")
+
+}
+})
